497135ec6a16ba09a2e2e6c66b591015cbc89253
Andoni Sanz <andonisanzalcalde@gmail.com>
2023-03-14 09:42:21 +0100

    First commit: Application and Login Controller scaffolds

---
+++ src/1dami/tartangamngmt/controller/LoginController.java
@@ -0,0 +1,12 @@
+package 1dami.tartangamngmt.app;
+/*
+*Main class for the Tartanga's Management application
+*/
+public class Application{
+	/*
+	*Entry point for the application
+	*/
+	public static void main (String args){
+	//this methos does nothing
+	}
+}
